f925684485b585cdd0cebe303e64b069fd5aedde
N1ghtmarecus <maciej.lubaszka.88@gmail.com>
2024-05-18 22:48:37 +0200

    Dependency Injection and Service Registration

---
+++ WebAPI/Installers/SwaggerInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebAPI.Installers
+{
+    public class SwaggerInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(x =>
+            {
+                x.EnableAnnotations();
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,9 +1,4 @@
-
-using Application.Interfaces;
-using Application.Mappings;
-using Application.Services;
-using Domain.Interfaces;
-using Infrastructure.Repositories;
+using WebAPI.Installers;
 
 namespace WebAPI;
 
@@ -13,19 +8,9 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
-        builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(c =>
-        {
-            c.EnableAnnotations();
-        });
-
-        builder.Services.AddScoped<IBoxRepository, BoxRepository>();
-        builder.Services.AddScoped<IBoxService, BoxService>();
-        builder.Services.AddSingleton(AutoMapperConfig.Initialize());
+        builder.Services.InstallServicesInAssembly(builder.Configuration);
 
         var app = builder.Build();
 
@@ -39,7 +24,6 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-
 
         app.MapControllers();
 
