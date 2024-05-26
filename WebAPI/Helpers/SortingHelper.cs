c41da6730f0ec3a33cf67a02db27a4eef1d72040
N1ghtmarecus <maciej.lubaszka.88@gmail.com>
2024-05-26 21:08:03 +0200

    Implement Sorting Functionality - Part III (Extend Box Sorting by Length, Width, and Height)

--- WebAPI/Helpers/SortingHelper.cs
+++ WebAPI/Helpers/SortingHelper.cs
@@ -4,13 +4,16 @@
     {
         public static KeyValuePair<string, string>[] GetSortField()
         {
-            return new[] { SortFields.CutterID, SortFields.CreationDate };
+            return [SortFields.CutterID, SortFields.Length, SortFields.Width, SortFields.Height, SortFields.CreationDate];
         }
     }
 
     public class SortFields
     {
         public static KeyValuePair<string, string> CutterID { get; } = new("cutterID", "CutterID");
+        public static KeyValuePair<string, string> Length { get; } = new("length", "Length");
+        public static KeyValuePair<string, string> Width { get; } = new("width", "Width");
+        public static KeyValuePair<string, string> Height { get; } = new("height", "Height");
         public static KeyValuePair<string, string> CreationDate { get; } = new("creationdate", "Created");
     }
 }
